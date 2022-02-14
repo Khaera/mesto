18fc1598d8e5ab65a474583c4014c8747160ed27
Khaera <sf13371@yandex.ru>
2022-02-14 14:30:07 +0300

    правки после 1-го код-ревью 	modified:   blocks/element/__image/element__image.css 	modified:   blocks/elements/__list/elements__list.css 	modified:   blocks/elements/elements.css 	modified:   blocks/footer/__text/footer__text.css 	modified:   blocks/popup/__container/popup__container.css 	new file:   blocks/popup/__info/_edit/_career/popup__info_edit_career.css 	new file:   blocks/popup/__info/_edit/_name/popup__info_edit_name.css 	renamed:    blocks/popup/__name/popup__name.css -> blocks/popup/__info/popup__info.css 	modified:   blocks/popup/_opened/popup_opened.css 	modified:   blocks/popup/popup.css 	modified:   blocks/profile/__add-button/profile__add-button.css 	modified:   blocks/profile/__avatar/profile__avatar.css 	modified:   blocks/profile/__career/profile__career.css 	modified:   blocks/profile/__edit-button/profile__edit-button.css 	modified:   blocks/profile/__name/profile__name.css 	modified:   index.html 	modified:   pages/index.css 	renamed:    pages/index.js -> scripts/index.js 	new file:   vendor/fonts/Inter-Medium.woff 	new file:   vendor/fonts/Inter-Medium.woff2 	modified:   vendor/fonts/inter.css

--- scripts/index.js
+++ scripts/index.js
@@ -2,37 +2,35 @@ let editButton = document.querySelector('.profile__edit-button');
 let popUp = document.querySelector('.popup');
 let closeButton = popUp.querySelector('.popup__close-button');
 let profileContainer = document.querySelector('.profile');
-let saveButton = popUp.querySelector('.popup__save-button');
 let profileName = profileContainer.querySelector('.profile__name');
 let profileCareer = profileContainer.querySelector('.profile__career');
-let nameInput = popUp.querySelector('.popup__name');
-let careerInput = popUp.querySelector('.popup__career');
+let nameInput = popUp.querySelector('.popup__info_edit_name');
+let careerInput = popUp.querySelector('.popup__info_edit_career');
 
 
 
 function openPopUp() {
   popUp.classList.add('popup_opened');
+  nameInput.setAttribute('value', profileName.textContent);
+  careerInput.setAttribute('value', profileCareer.textContent);
 }
-
-editButton.addEventListener('click', openPopUp);
 
 
 function closePopUp() {
   popUp.classList.remove('popup_opened');
 }
 
-closeButton.addEventListener('click', closePopUp);
-
 function formSubmitHandler (evt) {
   evt.preventDefault();
-  nameInput.getAttribute('value');
-  careerInput.getAttribute('value');
   profileName.textContent = nameInput.value;
   profileCareer.textContent = careerInput.value;
   closePopUp();
 }
 
+closeButton.addEventListener('click', closePopUp);
+editButton.addEventListener('click', openPopUp);
 popUp.addEventListener('submit', formSubmitHandler);
+
 
 
 
