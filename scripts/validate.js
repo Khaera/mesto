0887e823afcfbda4895924ef76868b4cec1fa4af
Khaera <sf13371@yandex.ru>
2022-03-06 16:53:17 +0300

    fix: redesigned validate.js

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,24 +1,24 @@
 //функция показа ошибки
-const showError = (formElement, inputElement, errorMessage, {inputErrorClass, errorClass}) => {
+const showError = (formElement, inputElement, errorMessage, configValidation) => {
   //переменная элемента ошибки
   const errorElement = inputElement.closest('.popup__field').querySelector('.popup__input-error');
 
   errorElement.textContent = errorMessage;
-  errorElement.classList.add(errorClass);
-  inputElement.classList.add(inputErrorClass);
+  errorElement.classList.add(configValidation.errorClass);
+  inputElement.classList.add(configValidation.inputErrorClass);
 };
 
 //функция скрытия ошибки
-const hideError = (formElement, inputElement, {inputErrorClass, errorClass}) => {
+const hideError = (formElement, inputElement, configValidation) => {
   const errorElement = inputElement.closest('.popup__field').querySelector('.popup__input-error');
 
   errorElement.textContent = '';
-  errorElement.classList.remove(errorClass);
-  inputElement.classList.remove(inputErrorClass);
+  errorElement.classList.remove(configValidation.errorClass);
+  inputElement.classList.remove(configValidation.inputErrorClass);
 };
 
 //функция проверки валидности инпута формы
-const checkFormValidity = (formElement, inputElement, rest) => {
+const checkFormValidity = (formElement, inputElement, configValidation) => {
   //переменная невалидного инпута
   const isInputInvalid = !inputElement.validity.valid;
 
@@ -27,15 +27,15 @@ const checkFormValidity = (formElement, inputElement, rest) => {
     //переменная сообщения ошибки с дефолтным текстом ошибки браузера
     const errorMessage = inputElement.validationMessage;
 
-    showError(formElement, inputElement, errorMessage, rest);
+    showError(formElement, inputElement, errorMessage, configValidation);
   } else {
-    hideError(formElement, inputElement, rest);
+    hideError(formElement, inputElement, configValidation);
   }
 };
 
 
 //функция переключения состояния кнопки отправить
-const toggleButtonState = (inputList, submitButtonElement, {inactiveButtonClass}) => {
+const toggleButtonState = (inputList, submitButtonElement, configValidation) => {
 
   //получаем невалидный инпут из списка всех инпутов
   const hasInvalidInput = Array.from(inputList).some((inputElement => {
@@ -44,26 +44,26 @@ const toggleButtonState = (inputList, submitButtonElement, {inactiveButtonClass}
 
   //условная конструкция, проверяющая валидный или невалидный инпут.
   if (hasInvalidInput) {
-    submitButtonElement.classList.add(inactiveButtonClass);//добавляет класс кнопке
+    submitButtonElement.classList.add(configValidation.inactiveButtonClass);//добавляет класс кнопке
     submitButtonElement.setAttribute('disabled', true); //добавляет атрибут disabled в html разметку кнопке
   } else {
-    submitButtonElement.classList.remove(inactiveButtonClass);
+    submitButtonElement.classList.remove(configValidation.inactiveButtonClass);
     submitButtonElement.removeAttribute('disabled');
   }
 };
 
 
 //функция устанавливающая список событий на все инпуты
-const setEventListeners = (formElement, {inputSelector, submitButtonSelector, ...rest}) => {
+const setEventListeners = (formElement, configValidation) => {
 
-  const inputList = Array.from(formElement.querySelectorAll(inputSelector)); //переменная списка инпутов
+  const inputList = Array.from(formElement.querySelectorAll(configValidation.inputSelector)); //переменная списка инпутов
 
-  const submitButtonElement = formElement.querySelector(submitButtonSelector); //переменная кнопки отправки формы
+  const submitButtonElement = formElement.querySelector(configValidation.submitButtonSelector); //переменная кнопки отправки формы
 
   inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', function () {
-      checkFormValidity(formElement, inputElement, rest);
-      toggleButtonState(inputList, submitButtonElement, rest);
+      checkFormValidity(formElement, inputElement, configValidation);
+      toggleButtonState(inputList, submitButtonElement, configValidation);
     });
   });
 };
@@ -79,29 +79,32 @@ const checkActualValidation = (popup) => {
 
     //прорка валидности инпута и если валидно, ошибка скрывается.
     if (inputElement.validity.valid) {
-      hideError(popup, inputElement, {inputErrorClass: 'popup__input_invalid', errorClass: 'popup__input-error_active'});
+      hideError(popup, inputElement, configValidation);
     }
 
-    toggleButtonState(inputList, submitButtonElement, {inactiveButtonClass: 'popup__save-button_disabled'});
+    toggleButtonState(inputList, submitButtonElement, configValidation);
   })
 }
 
 
 //функция включения валидации, принимающая на вход объект настроек.
-const enableValidation = ({formSelector, ...rest}) => {
-  const formsList = Array.from(document.querySelectorAll(formSelector));
+const enableValidation = (configValidation) => {
+  const formsList = Array.from(document.querySelectorAll(configValidation.formSelector));
   formsList.forEach(form => {
-    setEventListeners(form, rest);
+    setEventListeners(form, configValidation);
   });
 };
 
-//вызов функции включения валидации форм, которая принимает в себя объект данных
-enableValidation({
+//объект настроек валидации формы
+const configValidation = {
   formSelector: '.popup__form',
   inputSelector: '.popup__input',
   submitButtonSelector: '.popup__save-button',
   inactiveButtonClass: 'popup__save-button_disabled',
   inputErrorClass: 'popup__input_invalid',
   errorClass: 'popup__input-error_active',
-});
+};
+
+//вызов функции включения валидации форм, которая принимает в себя объект данных
+enableValidation(configValidation);
 
